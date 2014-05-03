c90d84ad132302596fd2be0dbb7f0a870492286e
Muk4 <muk4.muk4@gmail.com>
2014-05-03 13:36:10 +0200

    initial commit

---
+++ Example/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check source code for examples. Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        // Simple page scraping
+        public static void Example1()
+        {
+            Muka.HttpRequest request;
+
+            request = new Muka.HttpRequest("http://example.com");
+            // or using Uri object
+            Uri uri = new Uri("http://example.com");
+            request = new Muka.HttpRequest(uri);
+
+            string source = request.DownloadString();
+            Console.WriteLine(source);
+        }
+
+        // Download files from web (e.g, images, documents)
+        public static void Example2()
+        {
+            var request = new Muka.HttpRequest("http://upload.wikimedia.org/wikipedia/commons/7/70/Example.png");
+            int fileSize = request.DownloadFile(@"C:\Example.png");
+            Console.WriteLine("Download complete. Downloaded {0} bytes.", fileSize);
+        }
+
+        // Page download with WebException suppressed 
+        // If WebException is thrown DownloadString() returns empty string
+        public static void Example3()
+        {
+            var request = new Muka.HttpRequest("http://this.page.doesnt.exists");
+            request.SupressWebExceptions = true;
+            string source = request.DownloadString();
+            Console.WriteLine("Source length: {0} bytes.", source.Length);
+        }
+
+        // Send post data and use cookies
+        public static void Example4()
+        {
+            var request = new Muka.HttpRequest("http://example.com/login");
+            // you can use dictionary
+            request.PostData.Add("login", "darthvader");
+            request.PostData.Add("password", "deathstar");
+            // or raw string (it will be encoded internally) - you can use both methods simultaneously if you want to
+            request.RawPostData = "var1=smth1&var2=smth2";
+
+            string source = request.DownloadString();
+            // you can check source to deteremine if you're logged in or not
+            if (source.Contains("logout"))
+            {
+                var request2 = new Muka.HttpRequest("http://example.com/messages");
+                // set cookies
+                request2.Cookies = request.Cookies;
+                string source2 = request.DownloadString();
+                // process user's messages
+                // [...]
+            }
+        }
+
+        // Set headers and customize HttpWebRequest
+        public static void Example5()
+        {
+            var request = new Muka.HttpRequest("http://example.com/");
+            // you can work with underlying HttpWebRequest if you need more customization
+            // it's read-only so you can't set your own HttpWebRequest
+            request.Request.Headers.Add(HttpRequestHeader.ContentType, "application/pdf");
+            request.Request.UserAgent = "customUA";
+
+            string source = request.DownloadString();
+        }
+    }
+}
---
+++ HttpRequest/HttpRequest.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace Muka
+{
+    public class HttpRequest
+    {
+        #region Public Properties
+
+        public HttpWebRequest Request
+        {
+            get { return _request; }
+        }
+
+        public CookieContainer Cookies
+        {
+            get { return _cookieJar; }
+            set { _cookieJar = value; }
+        }
+
+        public bool SupressWebExceptions
+        {
+            get { return _supressWebExceptions; }
+            set { _supressWebExceptions = value; }
+        }
+
+        public Encoding DefaultEncoding
+        {
+            get { return _defaultEncoding; }
+            set { _defaultEncoding = value; }
+        }
+
+        public string RawPostData
+        {
+            get { return _rawPostData; }
+            set { _rawPostData = value; }
+        }
+
+        public Dictionary<string, string> PostData
+        {
+            get { return _postData; }
+            set { _postData = value; }
+        }
+
+        #endregion
+
+        #region Private Properties
+
+        private HttpWebRequest _request;
+        private CookieContainer _cookieJar;
+        private bool _supressWebExceptions;
+        private Encoding _defaultEncoding;
+        private string _rawPostData;
+        private Dictionary<string, string> _postData;
+
+        #endregion
+
+        #region Constructors
+
+        public HttpRequest(string url) : this(new Uri(url)) { }
+
+        public HttpRequest(Uri uri)
+        {
+            _supressWebExceptions = false;
+            _defaultEncoding = Encoding.UTF8;
+            _rawPostData = string.Empty;
+            _postData = new Dictionary<string, string>();
+
+            _request = (HttpWebRequest)WebRequest.Create(uri);
+            _request.UserAgent = "Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0";
+            _request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            _request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
+
+            _cookieJar = new CookieContainer();
+            _request.CookieContainer = _cookieJar;
+
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public string DownloadString()
+        {
+            PreparePost();
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)_request.GetResponse())
+                {
+                    _cookieJar.Add(response.Cookies);
+
+                    Encoding responseEncoding;
+                    try
+                    {
+                        responseEncoding = Encoding.GetEncoding(response.CharacterSet);
+                    }
+                    catch(ArgumentException)
+                    {
+                        responseEncoding = _defaultEncoding;
+                    }
+
+                    using (Stream dataStream = response.GetResponseStream())
+                    using (StreamReader reader = new StreamReader(dataStream, responseEncoding))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException)
+            {
+                if (_supressWebExceptions)
+                    return string.Empty;
+                else
+                    throw;
+            }
+        }
+
+        public int DownloadFile(string filePath)
+        {
+            byte[] buffer = new byte[1024];
+            int fileSize = 0;
+            int bytesRead = 0;
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)_request.GetResponse())
+                {
+                    _cookieJar.Add(response.Cookies);
+
+                    using (Stream responseStream = response.GetResponseStream())
+                    using (Stream fileStream = File.Create(filePath))
+                    {
+                        do
+                        {
+                            bytesRead = responseStream.Read(buffer, 0, buffer.Length);
+                            fileStream.Write(buffer, 0, bytesRead);
+                            fileSize += bytesRead;
+                        } while (bytesRead > 0);
+                    }
+                }
+
+                return fileSize;
+            }
+            catch (WebException)
+            {
+                if (_supressWebExceptions)
+                    return -1;
+                else
+                    throw;
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private void PreparePost()
+        {
+            if(_rawPostData.Length > 0 || _postData.Count > 0)
+            {
+                _request.Method = "POST";
+
+                string post = string.Empty;
+
+                foreach (KeyValuePair<string, string> kvp in _postData)
+                    post += string.Format("{0}={1}&", Uri.EscapeUriString(kvp.Key), Uri.EscapeUriString(kvp.Value));
+
+                post += _rawPostData;
+
+                byte[] byteArray = Encoding.UTF8.GetBytes(post);
+                _request.ContentType = "application/x-www-form-urlencoded";
+                _request.ContentLength = byteArray.Length;
+
+                using (Stream dataStream = _request.GetRequestStream())
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+            }
+        }
+
+        #endregion
+    }
+
+
+}
